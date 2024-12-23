8792e85329f5576c5881e73c6991e7ec08bd155f
Vladyslav Popov <popovvladislav4891@gmail.com>
2024-12-23 14:19:58 +0200

    Add examples

--- Labs/task2.js
+++ Labs/task2.js
@@ -38,3 +38,18 @@ const asyncMapPromise = (array, changeElement) => {
   });
 };
 
+asyncMapPromise([1, 2, 3, 4, 5], element => element * 2)
+    .then(result => console.log('promise based result:', result))
+    .catch(error => console.error(error));
+
+async function example1() {
+    try {
+        const data = [6, 7, 8, 9, 10];
+        const result = await asyncMapPromise(data, element => element * 2);
+        console.log('async-await result:', result);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+example1();
