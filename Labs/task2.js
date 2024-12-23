fc45a81f872b35d27f5e1d37ae1be21d257ab40c
Vladyslav Popov <popovvladislav4891@gmail.com>
2024-12-23 20:41:40 +0200

    minor changes

--- Labs/task2.js
+++ Labs/task2.js
@@ -12,7 +12,7 @@ const asyncMapPromise = (array, changeElement, concurrency) => {
     const result = [];
     let activePromises = 0;
 
-    function handleNextElement() {
+    const handleNextElement = () => {
       while (activePromises < concurrency && index < array.length) {
         const currentIndex = index++;
         activePromises++;
@@ -21,14 +21,16 @@ const asyncMapPromise = (array, changeElement, concurrency) => {
         if (logDurationTime) {
           console.log(`Starting task for element ${array[currentIndex]}`);
         }
-        
+
         Promise.resolve(changeElement(array[currentIndex]))
           .then((mappedElement) => {
             const durationTime = Date.now() - start;
             if (logDurationTime) {
-              console.log(`Finished task for element ${array[currentIndex]}, took ${durationTime}ms`);
+              console.log(
+                `Finished task for element ${array[currentIndex]}, took ${durationTime}ms`
+              );
             }
-            
+
             result[currentIndex] = mappedElement;
             activePromises--;
             handleNextElement();
@@ -51,7 +53,7 @@ const asyncMapPromise = (array, changeElement, concurrency) => {
         }
         resolve(result);
       }
-    }
+    };
 
     handleNextElement();
   });
@@ -60,7 +62,9 @@ const asyncMapPromise = (array, changeElement, concurrency) => {
 asyncMapPromise(
   [1, 2, 3, 4, 5, 6],
   (element) =>
-    new Promise((resolve) => setTimeout(() => resolve(element * 2), 1000)), 2)
+    new Promise((resolve) => setTimeout(() => resolve(element * 2), 1000)),
+  2
+)
   .then((result) => console.log("Promise-based result:", result))
   .catch((error) => console.error(error));
 
@@ -70,7 +74,9 @@ async function example1() {
     const result = await asyncMapPromise(
       data,
       (element) =>
-        new Promise((resolve) => setTimeout(() => resolve(element * 2), 1000)), 2);
+        new Promise((resolve) => setTimeout(() => resolve(element * 2), 1000)),
+      2
+    );
     console.log("Async-await result:", result);
   } catch (error) {
     console.error("Error:", error);
