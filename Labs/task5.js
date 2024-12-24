f14aa3725d12c59077a6fe29c38687240e14cf34
Vladyslav Popov <popovvladislav4891@gmail.com>
2024-12-24 20:08:59 +0200

    Add more examples

--- Labs/task5.js
+++ Labs/task5.js
@@ -11,11 +11,32 @@ class Notifier {
   }
 }
 
+class Logger {
+  constructor(hub) {
+    hub.on("message", (data) => {
+      console.log(`[Logger]: Recieved new message - ${data.type}`);
+    });
+  }
+}
+
+class ErrorHandler {
+  constructor(hub) {
+    hub.on("error", (data) => {
+      console.error(`[Error]: ${data.content}`);
+    });
+  }
+}
+
 const notifier = new Notifier(messageHub);
+const logger = new Logger(messageHub);
+const errorHandler = new ErrorHandler(messageHub);
 
 function sendMessage(type, content) {
   messageHub.emit(type, { type, content });
+  messageHub.emit("message", { type, content });
 }
 
 sendMessage("notification", "You have new message");
 sendMessage("notification", "Your task is completed");
+sendMessage("error", "Database connection failure.");
+sendMessage("error", "Not enough memory.");
