0808ac0f3042e1b19f2a1a1b4bd869e1bad20555
Vladyslav Popov <popovvladislav4891@gmail.com>
2024-12-28 17:00:27 +0200

    Add errorHandler in callback

--- Labs/task1.js
+++ Labs/task1.js
@@ -1,59 +1,68 @@
 const asyncMap = (array, callback, onComplete) => {
-    const minExecutionTime = 100;
-    const delay = 3000;
-    const startTime = Date.now();
-    const result = [];
-    let index = 0;
-  
-    const errorHandler = (message) => {
-      const error = new Error(message);
-      console.error('Error during mapping:', error);
-    };
-      
-    if (!Array.isArray(array)) {
-      return errorHandler('Data type must be an array');
+  const minExecutionTime = 100;
+  const delay = 3000;
+  const startTime = Date.now();
+  const result = [];
+  let index = 0;
+
+  const errorHandler = (message) => {
+    const error = new Error(message);
+    console.error('Error during mapping:', error);
+  };
+
+  if (!Array.isArray(array)) {
+    return errorHandler('Data type must be an array');
+  }
+
+  const handleNextElement = () => {
+    if (index >= array.length) {
+      const endTime = Date.now();
+      const totalTime = endTime - startTime;
+
+      if (totalTime < minExecutionTime) {
+        return setTimeout(() => {
+          onComplete(result);
+        }, delay);
+      }
+
+      return onComplete(result);
     }
-    
-    const handleNextElement = () => {
-      if (index >= array.length) {
-        const endTime = Date.now();
-        const totalTime = endTime - startTime;
-  
-        if (totalTime < minExecutionTime) {
-          return setTimeout(() => {
-            onComplete(result);
-          }, delay);
+
+    const element = array[index];
+    callback(element, (err, changedElement) => {
+      if (err) {
+        errorHandler(err);
+        return;
+      }
+
+      result.push(changedElement);
+      index++;
+      handleNextElement();
+    });
+  };
+
+  handleNextElement();
+};
+
+const asyncMapExample = () => {
+  const data = [1, 2, "NaN", 4, 5];
+
+  asyncMap(
+    data,
+    (element, done) => {
+      setTimeout(() => {
+        if (typeof element !== "number") {
+          return done(new Error(`"${element}" is not a number!`), null);
         }
-  
-        return onComplete(result);
-      }
-  
-      const element = array[index];
-      callback(element, (changedElement) => {
-        result.push(changedElement);
-        index++;
-        handleNextElement();
-      });
-    };
-    
-    handleNextElement();
-  };
-  
-  const asyncMapExample = () => {
-    const data = [1, 2, 3, 4, 5];
-    asyncMap(
-      data,
-      (element, handleMappedElement) => {
-        setTimeout(() => {
-          const change = element * 2;
-          handleMappedElement(change);
-        }, 10);
-      },
-      (result) => {
-        console.log("asyncMap - result:", result);
-      }
-    );
-  };
-  
-  asyncMapExample();
-  
+
+        const changedElement = element * 2;
+        done(null, changedElement);
+      }, 10);
+    },
+    (result) => {
+      console.log("asyncMap - result:", result);
+    }
+  );
+};
+
+asyncMapExample();
